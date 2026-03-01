f58bb5b13b133e8688b251158aa2be7f3dc88465
mangalam-srv <mangalamstp2005@gmail.com>
2026-03-01 15:27:12 +0530

    Auth

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'    
+
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 createRoot(document.getElementById('root')).render(
 
@@ -11,7 +20,13 @@ createRoot(document.getElementById('root')).render(
 // Navigate between pages
 // Render different components
 // Without refreshing the browser
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+
+    <BrowserRouter>
+     <App />
+   </BrowserRouter>,      
+  
+  </ClerkProvider>
+
 )
+  
