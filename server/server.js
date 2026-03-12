512851ba6495d330c9034a7a2d131dcd33f496d8
mangalam-srv <mangalamstp2005@gmail.com>
2026-03-12 18:59:04 +0530

    all ai features functions done

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import 'dotenv/config';
 import { clerkMiddleware, requireAuth } from '@clerk/express';
 import aiRouter from './routes/airoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(clerkMiddleware());
 app.get('/', (req,res)=>res.send('Server is live'));
 
 app.use('/api/ai', requireAuth(), aiRouter);
+app.use('/api/user', requireAuth(), userRouter);
 
 const PORT = process.env.PORT || 3000;
 
